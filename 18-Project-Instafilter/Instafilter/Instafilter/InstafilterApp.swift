0b633f7bf5a9df1d9f97491c97b40f2f42039496
influous <tobias.d.weber@mailbox.org>
2025-05-09 22:04:38 +0100

    Day 66: Add project

---
+++ 18-Project-Instafilter/Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Toto on 06/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
