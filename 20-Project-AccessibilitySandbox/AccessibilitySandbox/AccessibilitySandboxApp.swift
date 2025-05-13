3ca6ecae54411a7da3be0144f00ef8b2720edcac
influous <tobias.d.weber@mailbox.org>
2025-05-13 22:32:33 +0100

    Day 74: Add project

---
+++ 20-Project-AccessibilitySandbox/AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by Toto on 13/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
